d347e5d08d8cf248045562c163f233aaef3643b3
Jenny Zhu <jz2915@nyu.edu>
2019-04-14 23:26:27 +0800

    04/14/2019

---
+++ InteractiveComics-Paul/s4/js/scripts.js
@@ -0,0 +1,99 @@
+var scene = "";
+var section = "";
+var b4_1_1 = false, b4_1_2 = false, b4_1_3 = false;
+var arrow=false;
+var b4_2_1=false;
+var b4_3_1=false,b4_3_2=false,b4_3_3=false;
+var scroll=false;
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+function popBubble(){
+  var y = window.scrollY;
+  console.log(y);
+  var h = document.body.scrollHeight;
+  if(y<=400){
+    scene = "s4-1-";
+    if(y>5 && y<=150){
+      section = "1";
+      popup(scene,section,b4_1_1);
+      b4_1_1=true;
+    }else if(y>120 && y<=200){
+      section = "2";
+      popup(scene,section,b4_1_2);
+      b4_1_2=true;
+    }else if(y>280 && y<=340){
+      section = "3";
+      slideIn(scene,section,b4_1_3);
+      b4_1_3=true;
+    }
+  }
+  else if(y>2100 && y<=2200){
+      if(scroll===false){
+        document.body.style.overflowY=('hidden');
+      }
+  }
+  else if(y>2200 && y<=2400){
+    scene = "s4-2-";
+      section = "1";
+      popup(scene,section,b4_2_1);
+      b4_2_1=true;
+  }
+  else if(y>3800){
+    scene = "s4-3-";
+    if(y>3800 && y<=4300){
+      section = "1";
+      ringing(scene,section,b4_3_1);
+      b4_3_1=true;
+    }else if(y>4500 && y<=5000){
+      section = "2";
+      popup(scene,section,b4_3_2);
+      b4_3_2=true;
+      section2="3";
+      popup(scene,section2,b4_3_3);
+      b4_3_3=true;
+    }
+  }
+}
+
+function popup(scene,section,b){
+  if(b===false){
+    var popup = document.getElementById(scene+section);
+    popup.classList.toggle("show");
+  }
+}
+
+function ringing(scene,section,b){
+   if(b===false){
+     var ringing = document.getElementById(scene+section);
+     ringing.classList.toggle("ring");
+   }
+ }
+
+function camera(){
+   scroll=true;
+   document.getElementById("arrow").style.opacity=("1");
+   document.body.style.overflowY=('auto');
+ }
+
+function slideIn(scene,section,b){
+  if (b===false){
+    var img = document.getElementById(scene+section);
+    img.style.animation="slideIn 2s";
+    img.style.WebkitAnimation="slideIn 2s";
+    img.style.left="20%";
+    }
+  }
+
+function phoneRing() {
+  var y = window.scrollY;
+
+  if (y > 3800 && y<=5700) {
+    document.getElementById("player").play();
+  }
+  else {
+    document.getElementById("player").pause();
+  }
+}
---
+++ InteractiveComics-Paul/s5-1/js/scripts.js
@@ -0,0 +1,51 @@
+var scene = "";
+var section = "";
+var b5_1_2 = false, b5_1_4 = false, b5_1_5 = false;
+var bFall=false;
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
+function popBubble(){
+  var y = window.scrollY;
+  console.log(y);
+  var h = document.body.scrollHeight;
+  scene = "s5-1-";
+  if(y>1000 && y<=1300){
+    section = "2";
+    popup(scene,section,b5_1_2);
+    b5_1_2=true;
+  }else if(y>2600 && y<=3000){
+    section = "4";
+    popup(scene,section,b5_1_4);
+    b5_1_4=true;
+    if(b5_1_4===true){
+      bFall=true;
+      fall(scene,section,bFall);
+    }
+    bFall=false;
+  }else if(y>3700 && y<=h){
+    section = "5";
+    popup(scene,section,b5_1_5);
+    b5_1_5=true;
+  }
+}
+
+function popup(scene,section,b){
+  if(b===false){
+    var popup = document.getElementById(scene+section);
+    popup.classList.toggle("show");
+  }
+}
+
+function fall(scene,section,b){
+  if (b===true){
+    var img = document.getElementById(scene+section);
+    img.style.animation="fall 2.5s";
+    img.style.WebkitAnimation="fall 2.5s";
+    img.style.left="550px";
+    img.style.top="250px";
+    img.style.width="30%";
+    }
+  }
